9636d9ba1d2da39e8b3527280e8283eb80658cd5
Péter Torma <ptorma2011@gmail.com>
2016-04-20 20:06:47 +0200

    async repair, delete homework

--- homework_async/TP_AsyncCall/TP_AsyncCall-ejb/src/main/java/hu/codingmentor/LoggerProducer.java
+++ homework_async/TP_AsyncCall/TP_AsyncCall-ejb/src/main/java/hu/codingmentor/LoggerProducer.java
@@ -7,8 +7,7 @@ import javax.enterprise.inject.spi.InjectionPoint;
 public class LoggerProducer {
     
     @Produces 
-    public Logger produceLogger(InjectionPoint injectionPoint)
-    {
+    public Logger produceLogger(InjectionPoint injectionPoint) {
         return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
     }
 }
